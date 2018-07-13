6474b8064db20d872cc720c0cc4afb8059c730a6
Vincent Wijaya <weqqss@yahoo.com>
2018-07-13 13:43:48 +0700

    Fixing day 3 task

--- Phase-0/Week-3/Day 3/number-palindrome.js
+++ Phase-0/Week-3/Day 3/number-palindrome.js
@@ -1,12 +1,19 @@
 function angkaPalindrome(num) {
-  num++
-  var angka = num.toString();
+  // you can only write your code here!
+  var palindrome = false
+  var angka = ''
 
-  while(angka !== angka.split('').reverse().join('')){
-    num++;
-    angka = num.toString();
+  while (palindrome === false) {
+    num++
+    angka = num.toString()
+    var reverseAngka = ''
+
+    for (var i = angka.length - 1; i >= 0; i--) {
+      reverseAngka += angka[i]
+    }
+
+    palindrome = angka === reverseAngka
   }
-
   return num;
 }
 
@@ -16,4 +23,3 @@ console.log(angkaPalindrome(10)); // 11
 console.log(angkaPalindrome(117)); // 121
 console.log(angkaPalindrome(175)); // 181
 console.log(angkaPalindrome(1000)); // 1001
-console.log(angkaPalindrome(101));
