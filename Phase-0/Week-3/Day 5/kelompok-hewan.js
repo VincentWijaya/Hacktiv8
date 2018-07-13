d546cf8576962768dbc1ec821f5a29dc0b3c70a9
Vincent Wijaya <weqqss@yahoo.com>
2018-07-13 19:54:20 +0700

    Fixing day 5 task

--- Phase-0/Week-3/Day 5/kelompok-hewan.js
+++ Phase-0/Week-3/Day 5/kelompok-hewan.js
@@ -1,22 +1,31 @@
 function groupAnimals(animals) {
   // you can only write your code here!
-  animals.sort();
-  var firstAlphabet = animals[0][0]
-  var resultArr = []
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  var newAnimals = []
   var tempArr = []
+  var hasil = []
 
-  for (var i = 0; i < animals.length; i++) {
-    if (animals[i][0] === firstAlphabet) {
-      tempArr.push(animals[i])
-    } else {
-      resultArr.push(tempArr)
-      tempArr = []
-      firstAlphabet = animals[i][0]
-      tempArr.push(animals[i])
+  for (var i = 0; i < alphabet.length; i++) {
+    for (var j = 0; j < animals.length; j++) {
+      if (alphabet[i] === animals[j][0]) {
+        newAnimals.push(animals[j])
+      }
     }
   }
-  resultArr.push(tempArr)
-  return resultArr
+
+  for (var k = 0; k < newAnimals.length; k++) {
+    if (k === 0) {
+      tempArr.push(newAnimals[k])
+    } else if (k > 0 && newAnimals[k-1][0] === newAnimals[k][0]) {
+      tempArr.push(newAnimals[k])
+    } else if (k > 0 && newAnimals[k-1][0] !== newAnimals[k][0]) {
+      hasil.push(tempArr)
+      tempArr = []
+      tempArr.push(newAnimals[k])
+    }
+  }
+  hasil.push(tempArr)
+  return hasil
 }
 
 // TEST CASES
