adf0c46424723967caf33274b80533f6207e1b90
Vincent Wijaya <weqqss@yahoo.com>
2018-07-14 20:07:55 +0700

    Fixing day 5 task

--- Phase-0/Week-3/Day 5/kelompok-hewan.js
+++ Phase-0/Week-3/Day 5/kelompok-hewan.js
@@ -1,27 +1,27 @@
 function groupAnimals(animals) {
   // you can only write your code here!
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  var newAnimals = []
-  var tempArr = []
   var hasil = []
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  var sortedArr = []
+  var tempArr = []
 
   for (var i = 0; i < alphabet.length; i++) {
     for (var j = 0; j < animals.length; j++) {
       if (alphabet[i] === animals[j][0]) {
-        newAnimals.push(animals[j])
+        sortedArr.push(animals[j])
       }
     }
   }
 
-  for (var k = 0; k < newAnimals.length; k++) {
+  for (var k = 0; k < sortedArr.length; k++) {
     if (k === 0) {
-      tempArr.push(newAnimals[k])
-    } else if (k > 0 && newAnimals[k-1][0] === newAnimals[k][0]) {
-      tempArr.push(newAnimals[k])
-    } else if (k > 0 && newAnimals[k-1][0] !== newAnimals[k][0]) {
+      tempArr.push(sortedArr[k])
+    } else if (k > 0 && sortedArr[k][0] === sortedArr[k-1][0]) {
+      tempArr.push(sortedArr[k])
+    } else if (k > 0 && sortedArr[k][0] !== sortedArr[k-1][0]) {
       hasil.push(tempArr)
       tempArr = []
-      tempArr.push(newAnimals[k])
+      tempArr.push(sortedArr[k])
     }
   }
   hasil.push(tempArr)
