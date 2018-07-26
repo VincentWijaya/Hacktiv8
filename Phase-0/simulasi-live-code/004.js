9ec362a67624ef40cd0ab2eee85c9746ca81c88b
Vincent Wijaya <weqqss@yahoo.com>
2018-07-26 16:59:32 +0700

    Added simulasi LC

---
+++ Phase-0/simulasi-live-code/004.js
@@ -0,0 +1,49 @@
+/*
+==================================
+Array Mastery: Average Length Word
+==================================
+Nama:________
+[INSTRUKSI]
+Disediakan sebuah kalimat. Function averageLengthWord akan menerima satu parameter berupa string
+yang berisikan kalimat tersebut, dan akan mendapatkan rata-rata jumlah huruf dari setiap kata,
+kemudian mengembalikan nilai berupa array of string yang berisikan kata mana saja yang jumlahnya
+sama dengan rata-rata jumlah kata.
+
+NOTE:
+ - Spasi tidak dihitung
+ - Jika hasil rata-rata adalah desimal maka dibulatkan
+
+[CONTOH]
+input (kalimat): Do you want to become a great coder.
+rata-rata panjang kata dalam kalimat: 3
+output: ['you']
+
+input (kalimat): You dont know what you have until you lose it!.
+rata-rata panjang kata dalam kalimat: 4
+output: ['dont', 'know', 'what', 'have', 'lose']
+*/
+
+function averageLengthWord(words) {
+  // Code here
+  var split = words.split(' ')
+  var panjang = 0
+  var hasil = []
+
+  for (var i = 0; i < split.length; i++) {
+    panjang += split[i].length
+  }
+
+  var rataan = Math.round(panjang/split.length)
+
+  for (var j = 0; j < split.length; j++) {
+    if (split[j].length === rataan) {
+      hasil.push(split[j])
+    }
+  }
+
+  return hasil
+}
+
+console.log(averageLengthWord('Do you want to become a great coder ?')); // ['you']
+console.log(averageLengthWord('You dont know what you have until you lose it!')); // [ 'dont','know','what','have','lose']
+console.log(averageLengthWord('I am diligent')); // []
